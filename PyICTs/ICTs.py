688cbf5cb51f686a654c87145cf6eb2f79c54192
electrognu <electro.gnu@gmail.com>
2017-12-13 23:50:43 +0100

    Añadida carpeta PyICT donde tendremos nuestro programa

---
+++ PyICTs/ICTs.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from modulo_preguntas import *
+# Programa principal ... desde aquí se llamará a los modulos que haga falta
+# 
+
+
+amplificador()2
---
+++ PyICTs/modulo_preguntas.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def amplificador():
+	print(" Esto es del modulo preguntas")
+	G = input(" dime la ganancia del amplificador G = ")
+	F = input (" dime la figura de ruido del amplificador F = ")
+	return (G,F)
+
+
+def cable():
+	a = input("Dime la atenuacion del cable A = ")
+	return (a)
