53f8ceca4477d227b8e688ae2997123aa76c44eb
electrognu <electro.gnu@gmail.com>
2017-12-13 23:39:00 +0100

    Añadida la carpeta Ejemplos_basicos

---
+++ Ejemplos_Basicos/Ej1.py
@@ -0,0 +1,14 @@
+print("")
+print("Hola Mundo")
+print("primer programa")
+print("")
+print("El resultado de 4+5=", 4+5)
+print("El resultado de 4-5=", 4-5)
+
+
+
+print("")
+a = 57 + 100 + 20
+b = 10.10
+b = "par de cosas"
+print(b)
